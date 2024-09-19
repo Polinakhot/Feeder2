abe324005a6f7bfa2c52ea0bff71adbcc125e07b
Polinakhot <polinak9@nycstudents.net>
2024-09-19 13:27:48 +0000

    hi

--- Main.java
+++ Main.java
@@ -6,6 +6,17 @@ public class Main
     System.out.println(f);
     //System.out.println(f.currentFood);
     System.out.println(f.getCurrentFood());
+    Feeder g = new Feeder(500);
+    System.out.println(g.getCurrentFood());
+    g.simulateOneDay(12);
+    //System.out.println(Math.random());  = 0.248924827482......
+    //double random = Math.random() * 10;    
+    //System.out.println(random);  = 4.59375375......
+    //int random = (int) Math.random() * 10;
+    //System.out.println(random);   = 0
+    //int random = (int)(Math.random() * 10);
+    //System.out.println(random);    = 2
+    int random = (int)(Math.random() * 10 + 1);
+    System.out.println(random);
     }
-
 }
